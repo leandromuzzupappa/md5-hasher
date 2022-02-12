6a3f8742700ade8db225e346b3ec303e7b57c9d2
lmuzzupappa <lmuzzupappa@glamit.com.ar>
2022-02-12 19:13:09 -0300

    Seteando routas y controladores para mas placer :sparkles:

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Server levantado en el puerto ${PORT}`));
 
-app.get("/", (req, res) => {
-  return res.send("lenny was here!");
-});
+const mainRoutes = require("./routes/main-routes");
+
+app.use("/", mainRoutes);
 
 app.use(express.static("public"));
---
+++ controllers/main-controllers.js
@@ -0,0 +1,5 @@
+const mainControllers = {
+  index: (req, res) => res.send("Lenny was here overloaded"),
+};
+
+module.exports = mainControllers;
